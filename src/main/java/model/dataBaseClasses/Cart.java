58f1140503258941ba2133483f04135c52eac1db
shalaby <shalaby@shalaby-PC>
2018-02-27 06:19:33 +0200

    cart class

--- src/main/java/model/dataBaseClasses/Cart.java
+++ src/main/java/model/dataBaseClasses/Cart.java
@@ -10,6 +10,41 @@ package model.dataBaseClasses;
  * @author Ahmed
  */
 public class Cart {
-    int id;
-    //
+    private int id;
+    private Product product;
+    private int quantity;
+
+    public Cart() {
+    }
+
+    public Cart(int id, Product product, int quantity) {
+        this.id = id;
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
 }

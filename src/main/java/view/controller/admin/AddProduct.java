b4b51130223b4062b9d81707b18aa34d757ecee0
romaysaa <romaysaa.husien@google.com>
2018-03-06 13:42:32 +0200

    add produuct validation0

--- src/main/java/view/controller/admin/AddProduct.java
+++ src/main/java/view/controller/admin/AddProduct.java
@@ -23,6 +23,7 @@ public class AddProduct extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
+        
     }
 
 }
